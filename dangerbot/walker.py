ab426ec6757d0dec6af9e2d4aa16715d6dbbdcd8
reimannsum <15264176+reimannsum@users.noreply.github.com>
2019-01-23 09:05:12 -0500

    Finishing Suburb Dirs
    Adding the last two directions to the Suburb dir dictionary. these are needed now that forts have to be navigated around. at least one of the two were needed and it seemed silly to leave the last one out.

--- dangerbot/walker.py
+++ dangerbot/walker.py
@@ -26,7 +26,7 @@ def getPaths():
 class Suburb:
     plan = []
     # This is the Dictionary that will convert move directions into coodinates
-    dirs = {'U': [-1, 0], 'U-R': [-1, 1], 'U-L': [-1, -1], 'R': [0, 1], 'L': [0, -1], 'D': [1, 0]}
+    dirs = {'U': [-1, 0], 'U-R': [-1, 1], 'U-L': [-1, -1], 'R': [0, 1], 'L': [0, -1], 'D': [1, 0], 'D-R': [1, 1], 'D-L': [1, -1]}
 
     def set_plan(self, moves_list):
         self.plan = moves_list
