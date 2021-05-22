2a0143494d8be080cc3f9400626c8c64263967aa
eflynn <burkereimann@gmail.com>
2021-05-22 16:24:41 -0400

    formatting correction to assertion

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -15,7 +15,8 @@ Zombies:  Zed: 0  Ded:0
     assert walker1.write_log == """Log of MaltonMapper1 at [59, 62]
 Location: a warehouse [59, 62] in Tollyton
 AP: 33   Dead? True
-Condition: very strongly barricadedZombies:  Zed: 0  Ded:0
+Condition: very strongly barricaded
+Zombies:  Zed: 0  Ded:0
 """
 
 
